781159887cb044c2ccb0a82aa7beedf770a33b47
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-07-07 12:45:37 -0500

    Better Navbar
    Deleted old navbar replaced with bulma

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,122 @@
 // API Keys
 // var baGasKey = '1071d15d6cmsh24a0edab985b59ap1dfd1fjsn894de03a6f8b';
-var baAPI = '340f1157912d4ff6b27b91b2e968995f1c3a7802';
+var baAPI = "340f1157912d4ff6b27b91b2e968995f1c3a7802";
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function burgerMenu() {
-    var x = document.getElementById("myNavbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// navbar burger menu
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach((el) => {
+      el.addEventListener("click", () => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
   }
-
-
+});
