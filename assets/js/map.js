60dd9394e67c9619646c26784c5d818390d7ae36
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-07-12 13:11:20 -0500

    autocomplete working

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,10 +1,24 @@
-var heroEl = $(".hero");
-var mapEl = $("#map");
-var andrewA = "";
-var aeMap = $("#ae-map");
+// var heroEl = $(".hero");
+// var mapEl = $("#map");
+var andrewA = "AIzaSyCBrNRjKT6zvV08v1SM6-v-kbUtx-cYo8g";
+var destinationInput = document.getElementById('going-to');
+var originInput = document.getElementById('leaving');
+// var aeMap = $("#ae-map");
 
-function addMap() {
-  aeMap.append(
-    `<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed/v1/place?key=${andrewA}&q=Space+Needle,Seattle+WA"></iframe>`
-  );
-}
+
+// function addMap() {
+//   aeMap.append(
+//     `<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed/v1/place?key=${andrewA}&q=Space+Needle,Seattle+WA"></iframe>`
+//   );
+// }
+
+function initialize() {
+
+    new google.maps.places.Autocomplete(destinationInput);
+    new google.maps.places.Autocomplete(originInput);
+  }
+  
+  google.maps.event.addDomListener(window, 'load', initialize);
+  
+
+
