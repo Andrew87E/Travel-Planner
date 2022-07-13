96331cf73b6a8475617cf076b4d26213285ac621
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-07-13 02:14:31 -0500

    added modernizr need to add header cleaned up script merged spotify into index

--- assets/js/map.js
+++ assets/js/map.js
@@ -4,6 +4,7 @@
 3 styling
 4 pics
 5 polish
+6 add duration to gas card
 */
 
 var heroEl = $(".ae-hero");
@@ -28,14 +29,13 @@ var distance;
 var duration;
 var steps;
 var stepsArray;
+var spotEl = $(".ae-spot");
+spotEl.hide();
+
 // var destLatitude;
 // var destLongitude;
 // var weatherApiKey = "76dea1d2eaa53c39fea214a799bab840";
 // var weatherApiCall = `https://api.openweathermap.org/data/3.0/onecall?lat=${destLatitude}&lon=${destLongitude}&exclude={part}&appid=${weatherApiKey}`;
-
-function init() {
-  initalEl.hide();
-}
 
 $(document).keypress(function (event) {
   if (event.which == "13") {
@@ -48,6 +48,7 @@ $(document).keypress(function (event) {
 // }
 submitBtn.on("click", function (e) {
   e.preventDefault();
+  spotEl.show();
   var destination = destinationInput.value;
   var origin = originInput.value;
   var url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${andrewA}`;
@@ -118,50 +119,50 @@ submitBtn.on("click", function (e) {
       var stepsString = stepsArray.join("<br>");
       console.log(stepsString);
       generateFuelCost(distance, milesPerGallon);
-    //   generateSpotify();
+      //   generateSpotify();
     });
   // .catch(function (err) {
   //   console.log(err);
   // });
 
-//   function generateSpotify() {
-//     heroEl.append(
-//       '<div class="container ae-transparent spotify-card">' +
-//         "<h1>" +
-//         "Generate a playlist with Spotify!" +
-//         "</h1>" +
-//         '<form action="">' +
-//         '<input type="hidden" id="hidden_token">' +
-//         '<div class="col-sm-6 form-group row mt-4 px-0">' +
-//         '<label for="Genre" class="form-label col-sm-2">Genre:</label>' +
-//         '<select name="" id="select_genre" class="form-control form-control-sm col-sm-10">' +
-//         "<option>Select...</option>" +
-//         "</select>" +
-//         "</div>" +
-//         '<div class="col-sm-6 form-group row px-0">' +
-//         '<label for="Genre" class="form-label col-sm-2">Playlists:</label>' +
-//         '<select name="" id="select_playlist" class="form-control form-control-sm col-sm-10">' +
-//         "<option>Select...</option>" +
-//         "</select>" +
-//         "</div>" +
-//         '<div class="col-sm-6 row form-group px-0">' +
-//         '<button type="submit" id="btn_submit" class="btn btn-success col-sm-12">Search</button>' +
-//         "</div>" +
-//         "</form>" +
-//         '<div class="row">' +
-//         '<div class="col-sm-6 px-0">' +
-//         '<div class="list-group song-list">' +
-//         "</div>" +
-//         "</div>" +
-//         '<div class="offset-md-1 col-sm-4" id="song-detail">' +
-//         "</div>" +
-//         "</div>" +
-//         "</div>" +
-//         "</div>"
-//     );
-//     spotifyCardEl = $(".spotify-card");
-    
-//   }
+  //   function generateSpotify() {
+  //     heroEl.append(
+  //       '<div class="container ae-transparent spotify-card">' +
+  //         "<h1>" +
+  //         "Generate a playlist with Spotify!" +
+  //         "</h1>" +
+  //         '<form action="">' +
+  //         '<input type="hidden" id="hidden_token">' +
+  //         '<div class="col-sm-6 form-group row mt-4 px-0">' +
+  //         '<label for="Genre" class="form-label col-sm-2">Genre:</label>' +
+  //         '<select name="" id="select_genre" class="form-control form-control-sm col-sm-10">' +
+  //         "<option>Select...</option>" +
+  //         "</select>" +
+  //         "</div>" +
+  //         '<div class="col-sm-6 form-group row px-0">' +
+  //         '<label for="Genre" class="form-label col-sm-2">Playlists:</label>' +
+  //         '<select name="" id="select_playlist" class="form-control form-control-sm col-sm-10">' +
+  //         "<option>Select...</option>" +
+  //         "</select>" +
+  //         "</div>" +
+  //         '<div class="col-sm-6 row form-group px-0">' +
+  //         '<button type="submit" id="btn_submit" class="btn btn-success col-sm-12">Search</button>' +
+  //         "</div>" +
+  //         "</form>" +
+  //         '<div class="row">' +
+  //         '<div class="col-sm-6 px-0">' +
+  //         '<div class="list-group song-list">' +
+  //         "</div>" +
+  //         "</div>" +
+  //         '<div class="offset-md-1 col-sm-4" id="song-detail">' +
+  //         "</div>" +
+  //         "</div>" +
+  //         "</div>" +
+  //         "</div>"
+  //     );
+  //     spotifyCardEl = $(".spotify-card");
+
+  //   }
 
   function generateFuelCost() {
     if (fuelType === "premium-fuel") {
