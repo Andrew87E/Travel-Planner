ffe49c3d3ad2fa820c77d42d0c201b3229d6080b
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-07-12 22:38:03 -0500

    got gas card working
    FINALLY

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,33 +1,30 @@
-var heroEl = $(".hero");
+var heroEl = $(".ae-hero");
 var mapEl = $("#map");
 var andrewA = "AIzaSyCBrNRjKT6zvV08v1SM6-v-kbUtx-cYo8g";
 var destinationInput = document.getElementById("going-to");
 var originInput = document.getElementById("leaving");
 var aeMap = $("#ae-map");
 var submitBtn = $("#init-submit");
-// var mpg = $("#miles-per-gallon");
 var fuelTypeEl = $("#fuel-type");
 var defaultPremiumCost = 4.95;
 var defaultRegularCost = 4.45;
 var defaultDieselCost = 5.06;
 var mpg = $("#miles-per-gallon");
+var fuelArr = [];
+var regfuelCost = 0;
+var premFuelCost = 0;
+var dieselFuelCost = 0;
+var formEl = $(".login-box");
+
 var directions;
 var distance;
 var duration;
 var steps;
 var stepsArray;
-var destLatitude;
-var destLongitude;
-<<<<<<< Updated upstream
-var weatherApiKey = "46b9fbe392a7416271fab6f07e46740a";
-var weatherApiCall = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherApiKey}`;
-var cityName =document.getElementById("going-to");
-var currentCityEl = document.getElementById("weather-forecast");
-
-=======
-var weatherApiKey = "76dea1d2eaa53c39fea214a799bab840";
-var weatherApiCall = `https://api.openweathermap.org/data/3.0/onecall?lat=${destLatitude}&lon=${destLongitude}&exclude={part}&appid=${weatherApiKey}`;
->>>>>>> Stashed changes
+// var destLatitude;
+// var destLongitude;
+// var weatherApiKey = "76dea1d2eaa53c39fea214a799bab840";
+// var weatherApiCall = `https://api.openweathermap.org/data/3.0/onecall?lat=${destLatitude}&lon=${destLongitude}&exclude={part}&appid=${weatherApiKey}`;
 
 function init() {
   initalEl.hide();
@@ -53,28 +50,10 @@ submitBtn.on("click", function (e) {
       return response.json();
     })
     .then(function (data) {
-<<<<<<< Updated upstream
-        console.log(data);
-        var directions = data.routes[0].legs[0];
-         destLatitude = data.routes[0].legs[0].end_location.lat;
-         destLongitude = data.routes[0].legs[0].end_location.lng;
-        var distance = directions.distance.text;
-        var duration = directions.duration.text;
-        var steps = directions.steps;
-        var stepsArray = [];
-        for (var i = 0; i < steps.length; i++) {
-            stepsArray.push(steps[i].html_instructions);
-        }
-        console.log(stepsArray);
-        var stepsString = stepsArray.join("<br>");
-        console.log(stepsString);
-        var directionsEl = $(".directions");
-        directionsEl.html(`<h2>Directions</h2>
-=======
       console.log(data);
       var directions = data.routes[0].legs[0];
-      destLatitude = data.routes[0].legs[0].end_location[0].lat;
-      destLongitude = data.routes[0].legs[0].end_location[0].lng;
+      //   destLatitude = data.routes[0].legs[0].end_location[0].lat;
+      //   destLongitude = data.routes[0].legs[0].end_location[0].lng;
       var distance = directions.distance.text;
       var duration = directions.duration.text;
       var steps = directions.steps;
@@ -87,14 +66,13 @@ submitBtn.on("click", function (e) {
       console.log(stepsString);
       var directionsEl = $(".directions");
       directionsEl.html(`<h2>Directions</h2>
->>>>>>> Stashed changes
         <p>${stepsString}</p>
         <p>Distance: ${distance}</p>
         <p>Duration: ${duration}</p>`);
-    })
-    .catch(function (err) {
-      console.log(err);
     });
+  // .catch(function (err) {
+  //   console.log(err);
+  // });
 });
 // }
 
@@ -102,6 +80,7 @@ var mpg = $("#miles-per-gallon");
 
 submitBtn.on("click", function (e) {
   e.preventDefault();
+  formEl.hide();
   var fuelType = fuelTypeEl.val();
   if (fuelType === "null") {
     window.alert("Please select a fuel type");
@@ -131,24 +110,66 @@ submitBtn.on("click", function (e) {
       console.log(stepsArray);
       var stepsString = stepsArray.join("<br>");
       console.log(stepsString);
-      generateFuelCost(distance, mpg);
+      generateFuelCost(distance, milesPerGallon);
     });
   // .catch(function (err) {
   //   console.log(err);
   // });
+
   function generateFuelCost() {
     if (fuelType === "premium-fuel") {
-      var premFuelCost = (distance / mpg) * defaultPremiumCost;
+      generateFuelCostCard();
+      premFuelCost = (distance / milesPerGallon) * defaultPremiumCost;
       console.log(premFuelCost);
+      gasCardEl.append(
+        '<div class="gas-cost">' +
+          "<h2>" +
+          "Regular Fuel Cost: " +
+          "$" +
+          premFuelCost +
+          "</h2>" +
+          "</div>"
+      );
     } else if (fuelType === "regular-fuel") {
-      var regFuelCost = (distance / mpg) * defaultRegularCost;
+      generateFuelCostCard();
+      regFuelCost = (distance / milesPerGallon) * defaultRegularCost;
       console.log(regFuelCost);
+      gasCardEl.append(
+        '<div class="gas-cost">' +
+          "<h2>" +
+          "Premium Fuel Cost: " +
+          "$" +
+          regFuelCost +
+          "</h2>" +
+          "</div>"
+      );
     } else if (fuelType === "diesel-fuel") {
-      var dieselFuelCost = (distance / mpg) * defaultDieselCost;
+      generateFuelCostCard();
+      dieselFuelCost = (distance / milesPerGallon) * defaultDieselCost;
       console.log(dieselFuelCost);
+      gasCardEl.append(
+        '<div class="gas-cost">' +
+          "<h2>" +
+          "Diesel Fuel Cost: " +
+          "$" +
+          dieselFuelCost +
+          "</h2>" +
+          "</div>"
+      );
     }
   }
 });
+var gasCardEl;
+function generateFuelCostCard() {
+  heroEl.append(
+    '<div class="ae-transparent gas-card">' +
+      "<h1>" +
+      "Gas Cost" +
+      "</h1>" +
+      "</div>"
+  );
+  gasCardEl = $(".gas-card");
+}
 
 function initialize() {
   new google.maps.places.Autocomplete(destinationInput);
@@ -157,75 +178,26 @@ function initialize() {
 
 google.maps.event.addDomListener(window, "load", initialize);
 
-<<<<<<< Updated upstream
-
-function generateCurrentWeather() 
-  // $("#uvi").show();
-
-  currentCityEl.append(`  
-    <h1>
-      The current weather in  ${cityName}<br>
-      ${todaysDate}
-    </h1><br>
-    <p>
-      The current temp is: ${weatherCurrent.temp}<br>
-      It feels like: ${weatherCurrent.feels_like}<br>
-      The humidity is: ${weatherCurrent.humidity}<br>
-      The wind speed is: ${weatherCurrent.wind_speed}mph<br>    
-      <br><span id="uvi">The current UV Index is: ${Math.floor(
-        weatherCurrent.uvi
-      )}</span>
-    </p>
-  `);
-
-          function generateGeo() {
-            fetch(
-              `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`
-            )
-              .then(function (response) {
-                return response.json();
-              })
-              .then(function (data) {
-                console.log(data);
-                lat = data[0].lat;
-                lon = data[0].lon;
-                // })
-                // .then(function () {
-                var weatherAPIUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
-                fetch(weatherAPIUrl)
-                  .then(function (response) {
-                    return response.json();
-                  })
-                  .then(function (data) {
-                    weatherCurrent = data.current;
-                    weatherDaily = data.daily;
-                    generateCurrentWeather();
-                  });
-              });
-          }
-
-=======
-var weatherSearch = function () {
-  fetch(weatherApiCall).then(
-    function (response) {
-      return response.json();
-    }.then(function (data) {
-      for (var i = 0; i < data.length; i++) {
-        var currentWeather = data[i];
-        document.getElementById("#weather-forecast").innerHTML = currentWeather;
-      }
-    })
-  );
-};
-
-// document.getElementById('#init-submit').addEventListener('click', weatherSearch);
-var el = document.getElementById("#init-submit");
-if (el) {
-  el.addEventListener("click", swapper, false);
-}
->>>>>>> Stashed changes
-// function addMap() {
-//   aeMap.append(
-//     `<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed/v1/place?key=${andrewA}&q=Space+Needle,Seattle+WA"></iframe>`
+// var weatherSearch = function () {
+//   fetch(weatherApiCall).then(
+//     function (response) {
+//       return response.json();
+//     }.then(function (data) {
+//       for (var i = 0; i < data.length; i++) {
+//         var currentWeather = data[i];
+//         document.getElementById("#weather-forecast").innerHTML = currentWeather;
+//       }
+//     })
 //   );
+// };
+
+// // document.getElementById('#init-submit').addEventListener('click', weatherSearch);
+// var el = document.getElementById("#init-submit");
+// if (el) {
+//   el.addEventListener("click", swapper, false);
 // }
+// // function addMap() {
+// //   aeMap.append(
+// //     `<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed/v1/place?key=${andrewA}&q=Space+Needle,Seattle+WA"></iframe>`
+// //   );
+// // }
--- assets/js/script.js
+++ assets/js/script.js
@@ -69,125 +69,29 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-function generateFuelCost() {
-  hero.append(
-    '<div class="ae-transparent gas-cost">' +
-      "<h1>" +
-      "Gas Cost" +
-      "</h1>" +
-      "</div>"
-  );
-  var gasInfo = $(".gas-info");
-  if (regularFuel.prop("checked")) {
-    gasInfo.append(
-      '<div class="gas-cost">' +
-        "<h2>" +
-        "Regular Fuel Cost: " +
-        "$" +
-        // regFuelCost +
-        "</h2>" +
-        "</div>"
-    );
-  } else if (premiumFuel.prop("checked")) {
-    gasInfo.append(
-      '<div class="gas-cost">' +
-        "<h2>" +
-        "Premium Fuel Cost: " +
-        "$" +
-        // premFuelCost +
-        "</h2>" +
-        "</div>"
-    );
-  } else if (dieselFuel.prop("checked")) {
-    gasInfo.append(
-      '<div class="gas-cost">' +
-        "<h2>" +
-        "Diesel Fuel Cost: " +
-        "$" +
-        // dieselFuelCost +
-        "</h2>" +
-        "</div>"
-    );
-  }
-}
-
 // // function to hide
-function init() {
-  initalEl.hide();
-}
+// function init() {
+//   initalEl.hide();
+// }
 
-initSubmit.on("click", init);
+// initSubmit.on("click", init);
 
-<<<<<<< Updated upstream
-// var requestToken = document.querySelector('#obtain-new-token')
-// var userId = document.querySelector('#user-profile')
-// var createPlaylist = `https://api.spotify.com/v1/users/${userId}/playlists`
-// function createList (playlistname , public)
-// fetch (createPlaylist)
-// response = request.post(
-//   createPlaylist,
-//   headers = {
-//     'Authorization': 'Bearer', requestToken },
-//     json={
-//      'name': 'name',
-//       'public' : 'public',
-//   }
-// );
+// function renderCurrentForcast(weather, city, timeZone) {
+//   var { temperature, humidity, uvIndex } = weather;
+//   console.log(temperature.humidity, uvIndex);
+// }
 
-// function putPlaylist (){
-// var playlist = createList( 
-//   playlistname ='My Road Trip Playlist',
-//   public =  false
-// )
+// var weatherSearch = function () {
+//   fetch(weatherApiCall).then(
+//     function (response) {
+//       return response.json();
+//     }.then(function (data) {
+//       for (var i = 0; i < data.length; i++) {
+//         var currentWeather = data[i];
+//         document.getElementById("#weather-forecast").innerHTML = currentWeather;
+//       }
+//     })
+//   );
 // };
-// var requestToken = document.querySelector('#obtain-new-token')
-// var userId = document.querySelector('#user-profile')
-// var createPlaylist = `https://api.spotify.com/v1/users/${userId}/playlists`
-// function createList (name , public){
-// fetch (createPlaylist);
-// response = request.post(
-//   createPlaylist,
-//   headers = {
-//     'Authorization': 'Bearer', requestToken },
-//     json={
-//      name: 'name',
-//     public : 'public',
-//   }
-// )};
 
-// document.getElementById('#init-submit').addEventListener('click', weatherSearch());   
-//       function weatherSearch(){
-//       fetch(weatherApiCall)
-//       .then (function(response){
-//         console.log(response)
-//               return response.json()}
-//       .then (function(data){
-//        for (var i=0; i<data.length; i++){
-//        var currentWeather= data[i]
-//         document.getElementById('#weather-forecast').innerHTML = currentWeather
-//                          }
-//               })
-//           )};  
-
-
-=======
-function renderCurrentForcast(weather, city, timeZone) {
-  var { temperature, humidity, uvIndex } = weather;
-  console.log(temperature.humidity, uvIndex);
-}
-
-var weatherSearch = function () {
-  fetch(weatherApiCall).then(
-    function (response) {
-      return response.json();
-    }.then(function (data) {
-      for (var i = 0; i < data.length; i++) {
-        var currentWeather = data[i];
-        document.getElementById("#weather-forecast").innerHTML = currentWeather;
-      }
-    })
-  );
-};
-
-$("#init-submit").on("click", weatherSearch);
->>>>>>> Stashed changes
+// $("#init-submit").on("click", weatherSearch);
