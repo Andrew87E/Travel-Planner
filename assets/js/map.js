bf4aed88657d2f5227a1cbc070e5809b9fafb9d0
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-07-12 20:50:06 -0500

    added spotify playlist generator framework
    need to refine/style

--- assets/js/map.js
+++ assets/js/map.js
@@ -121,8 +121,10 @@ function initialize() {
 
 google.maps.event.addDomListener(window, "load", initialize);
 
-// function addMap() {
-//   aeMap.append(
-//     `<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed/v1/place?key=${andrewA}&q=Space+Needle,Seattle+WA"></iframe>`
-//   );
-// }
+function addMap() {
+  aeMap.append(
+    `<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed/v1/place?key=${andrewA}&q=Space+Needle,Seattle+WA"></iframe>`
+  );
+}
+
+addMap();
